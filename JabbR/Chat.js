ad59b09a895fab6d48a3217b722e2b83d13cd40e
Sijia Jiang <v-jiasi@XIAN2K8-V02.redmond.corp.microsoft.com>
2013-01-22 01:36:02 -0800

    change#2

--- JabbR/Chat.js
+++ JabbR/Chat.js
@@ -919,7 +919,7 @@
         // Initialize the ui, passing the user preferences
         ui.initialize(state.preferences);
 
-        ui.addMessage('Welcome to FOO branch' + originalTitle +' 01/22/2013', 'notification');
+        ui.addMessage('Welcome to FOO branch' + originalTitle +' Jessica. 01/22/2013', 'notification');
         ui.addMessage('Use ? or type /? to display the FAQ and list of commands', 'notification');
 
         function initConnection() {
